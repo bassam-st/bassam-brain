21fa69a857ae6dc6f35d47bf365b4481d907be37
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-10-06 08:24:46 +0300

    Update sw.js

--- static/pwa/sw.js
+++ static/pwa/sw.js
@@ -1,3 +1,14 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => self.clients.claim());
-self.addEventListener("fetch", () => {});
+self.addEventListener("install", e => {
+  console.log("بسام الذكي - Service Worker تم تثبيته");
+  e.waitUntil(
+    caches.open("bassam-cache").then(cache => {
+      return cache.addAll(["/", "/static/style.css"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
