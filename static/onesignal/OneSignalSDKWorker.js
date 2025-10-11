42ddae30d115a76b7efbbe06dbf0b0c1a6406fa1
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-10-12 00:46:37 +0300

    Update OneSignalSDKWorker.js

--- static/onesignal/OneSignalSDKWorker.js
+++ static/onesignal/OneSignalSDKWorker.js
@@ -1,3 +1,2 @@
-// static/onesignal/OneSignalSDKWorker.js
-// Worker الأساسي الخاص بإشعارات OneSignal
+// static/onesignal/OneSignalSDKUpdaterWorker.js
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
