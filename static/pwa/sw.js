478701fcf9f171f427bade007ac66bf2ca3801b4
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-10-12 00:41:09 +0300

    Update sw.js

--- static/pwa/sw.js
+++ static/pwa/sw.js
@@ -1,4 +1,2 @@
-// static/pwa/sw.js
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", (e) => self.clients.claim());
-self.addEventListener("fetch", () => {}); // بلا كاش — فقط لتفعيل التثبيت
+// static/sw.js
+importScripts('/static/onesignal/OneSignalSDKWorker.js');
